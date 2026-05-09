bc129d8145c34fdd685753bdcfd6be2aac1a7428
Thunuguntla Naga Vaishnavi <113518997+vaishnavithunuguntla@users.noreply.github.com>
2026-05-09 12:30:46 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://finance-backend-1-lvt3.onrender.com", 
+  baseURL: process.env.REACT_APP_API_URL, 
   headers: { "Content-Type": "application/json" }
 });
 
