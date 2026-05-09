cdb6751c732f22571a633a6d58554ee43bcb14d6
vaishnavi <suresh.thunuguntla78@gmail.com>
2026-05-09 12:20:34 +0530

    Initial commit of frontend project

--- src/api.js
+++ src/api.js
@@ -1,9 +1,8 @@
 // here we need to add our connection to fastapi
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://finance-backend-5ghn.onrender.com", 
+  baseURL: "https://finance-backend-1-lvt3.onrender.com", 
   headers: { "Content-Type": "application/json" }
 });
 
